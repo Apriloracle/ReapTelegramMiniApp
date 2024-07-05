7a8336252a79c7771feda5a2564ffb7d7678b90e
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-05 19:19:22 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useAccount, useBalance, useNetwork } from 'wagmi'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export const useWeb3 = () => {
+  const { address, isConnected } = useAccount()
+  const { chain } = useNetwork()
+  const { data: balance } = useBalance({
+    address: address,
+  })
+
+  return {
+    account: address,
+    isConnected,
+    balance: balance?.formatted,
+    chainId: ch
