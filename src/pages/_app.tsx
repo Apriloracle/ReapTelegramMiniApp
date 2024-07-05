cc92a0a283d33c77e56aecce5f91d7d8db149ed5
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-05 18:03:23 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Web3Provider from '@/components/Web3Provider'
+import { WagmiConfig } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { config } from '@/utils/config'
+
+// Create a client
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Web3Provider>
-      <Component {...pageProps} />
-    </Web3Provider>
+    <WagmiConfig config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiConfig>
   )
 }
