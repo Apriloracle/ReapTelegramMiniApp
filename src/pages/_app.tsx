5e93a84b18b11814ea27e2be2f6227d89f66b308
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-05 19:36:24 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from '@/utils/config'
 
-// Create a client
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
