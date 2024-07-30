2bdcbb606c951a31842f8cc98258281e1a59eef1
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-30 12:42:25 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { config } from '@/utils/config'
+import { ThirdwebProviderWrapper } from '../path/to/client'
+import TelegramMiniApp from '../path/to/components/TelegramMiniApp'
 
-const queryClient = new QueryClient()
-
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ThirdwebProviderWrapper>
+      <Component {...pageProps} />
+    </ThirdwebProviderWrapper>
   )
 }
+
+export default MyApp
