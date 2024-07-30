ba53c5973e8b33ba75aabb41996f9181a708491d
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-30 16:14:32 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThirdwebProviderWrapper } from '@/components/client'
-import TelegramMiniApp from '@/components/TelegramMiniApp'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <ThirdwebProviderWrapper>
-      <Component {...pageProps} />
-    </ThirdwebProviderWrapper>
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProviderWrapper>
+        <Component {...pageProps} />
+      </ThirdwebProviderWrapper>
+    </QueryClientProvider>
   )
 }
 
