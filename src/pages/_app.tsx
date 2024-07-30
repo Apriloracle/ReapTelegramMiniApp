96ab06dae1b86eb1e52be33ef9434ce29920411f
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-30 17:24:32 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThirdwebProviderWrapper } from '@/components/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react'
+import { Web3Provider } from '@/components/Web3Provider'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient())
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThirdwebProviderWrapper>
-        <Component {...pageProps} />
-      </ThirdwebProviderWrapper>
-    </QueryClientProvider>
+    <Web3Provider>
+      <Component {...pageProps} />
+    </Web3Provider>
   )
 }
 
