8eb952411a2d6992c118ffe1bbeee7edc4e71e8d
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-30 14:27:12 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThirdwebProviderWrapper } from '../path/to/client'
-import TelegramMiniApp from '../path/to/components/TelegramMiniApp'
+import { ThirdwebProviderWrapper } from '@/components/client'
+import TelegramMiniApp from '@/components/TelegramMiniApp'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
