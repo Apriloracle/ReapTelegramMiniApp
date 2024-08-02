6527eeba86bca6773f175d013d3d9e4404f0f0c5
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-02 12:02:03 -0400

    Update Web3Provider.tsx

--- src/components/Web3Provider.tsx
+++ src/components/Web3Provider.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { WagmiProvider, createConfig, http } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-import { celo } from 'wagmi/chains';
+import { Celo } from '@thirdweb-dev/chains';
 import { walletConnect } from 'wagmi/connectors';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 
@@ -10,9 +10,9 @@ const config = createConfig(
   getDefaultConfig({
     appName: 'Celo Telegram Mini App',
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [celo],
+    chains: [Celo],
     transports: {
-      [celo.id]: http()
+      [Celo.chainId]: http()
     },
     connectors: [
       walletConnect({ projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID! }),
@@ -24,7 +24,7 @@ const queryClient = new QueryClient();
 
 export const Web3Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <ThirdwebProvider activeChain={celo}>
+    <ThirdwebProvider activeChain={Celo}>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
           <ConnectKitProvider
