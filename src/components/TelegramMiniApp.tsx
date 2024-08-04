01ebfff8788d17bfc016b8fa7479159dfa4a2d6c
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-04 17:49:04 -0400

    Update TelegramMiniApp.tsx

--- src/components/TelegramMiniApp.tsx
+++ src/components/TelegramMiniApp.tsx
@@ -147,7 +147,7 @@ const TelegramMiniApp: React.FC = () => {
       }
 
       // Call the cloud function for ERC20 transfer
-      const response = await fetch('https://us-central1-fourth-buffer-421320.cloudfunctions.net/handleTap1', {
+      const response = await fetch('https://lobster-app-cighe.ondigitalocean.app/handleTap1', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
