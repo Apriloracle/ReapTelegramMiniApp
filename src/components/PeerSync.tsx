ca45700e94a59f64febd39b24b171b0a641de21c
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-08 13:21:27 -0400

    Update and rename WebSocketSync.tsx to PeerSync.tsx

---
+++ src/components/PeerSync.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, useCallback } from 'react';
+import { createMergeableStore } from 'tinybase';
+import { createWsSynchronizer } from 'tinybase/synchronizers/synchronizer-ws-client';
+
+interface PeerSyncProps {
+  onPeerCountUpdate?: (count: number) => void;
+}
+
+const PeerSync: React.FC<PeerSyncProps> = ({ onPeerCountUpdate }) => {
+  const [store] = useState(() => createMergeableStore());
+  const [synchronizer, setSynchronizer] = useState<any>(null);
+
+  useEffect(() => {
+    const initializeSync = async () => {
+      if (!store.hasTable('peers')) {
+        store.setTable('peers', { count: { value: 1 } });
+      }
+
+      const newSynchronizer = await createWsSynchronizer(
+        store,
+        new WebSocket('wss://todo.demo.tinybase.org')
+      );
+
+      setSynchronizer(newSynchronizer);
+
+      store.addCellListener('peers', 'count', 'value', (_, __, ___, ____, newValue) => {
+        if (onPeerCountUpdate) {
+          onPeerCountUpdate(newValue as number);
+        }
+      });
+
+      await newSynchronizer.startSync();
+    };
+
+    initializeSync();
+
+    return () => {
+      if (synchronizer) {
+        synchronizer.destroy();
+      }
+    };
+  }, [store, onPeerCountUpdate]);
+
+  const getPeerCount = useCallback(() => {
+    return store.getCell('peers', 'count', 'value') as number;
+  }, [store]);
+
+  return { getPeerCount };
+};
+
+export default PeerSync;
