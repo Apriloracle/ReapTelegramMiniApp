1cd376479cf29c8129c96c903ec7bdd1f43abe0e
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-08 19:25:10 -0400

    Update PeerSync.tsx

--- src/components/PeerSync.tsx
+++ src/components/PeerSync.tsx
@@ -19,7 +19,7 @@ const PeerSync: React.FC<PeerSyncProps> = ({ onPeerCountUpdate, onConnectionStat
       }
 
       try {
-        const webSocket = new WebSocket('wss://todo.demo.tinybase.org');
+        const webSocket = new WebSocket('wss://tinybasewsserver-production.up.railway.app');
 
         webSocket.onopen = () => {
           setIsConnected(true);
