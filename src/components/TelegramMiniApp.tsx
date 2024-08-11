e531d859fe519e08c2398049a69f0e7e649437e0
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-11 14:17:23 -0400

    Update TelegramMiniApp.tsx

--- src/components/TelegramMiniApp.tsx
+++ src/components/TelegramMiniApp.tsx
@@ -517,7 +517,6 @@ const TelegramMiniApp: React.FC = () => {
 
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/deals" element={<DealsComponent />} />
           <Route path="/deals" element={<DealsComponent localWalletAddress={localWalletAddress} />} />
         </Routes>
       </div>
