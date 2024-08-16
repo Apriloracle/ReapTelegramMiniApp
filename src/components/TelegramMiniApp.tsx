d369f41c3adb1991ed87feeb56b9b89e04ef8e8a
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-16 13:12:37 -0400

    Update TelegramMiniApp.tsx

--- src/components/TelegramMiniApp.tsx
+++ src/components/TelegramMiniApp.tsx
@@ -457,50 +457,48 @@ const TelegramMiniApp: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-    return (
-      <div style={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        display: 'flex',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        backgroundColor: '#1a1a1a',
-        height: '60px',
-        borderTop: '1px solid #333'
-      }}>
-        <button
-          onClick={() => navigate('/')}
-          style={{
-            background: 'none',
-            border: 'none',
-            color: location.pathname === '/' ? '#f05e23' : '#fff',
-            fontSize: '24px',
-            cursor: 'pointer',
-          }}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
-          </svg>
-        </button>
-        <button
-          onClick={() => navigate('/deals')}
-          style={{
-            background: 'none',
-            border: 'none',
-            color: location.pathname === '/deals' ? '#f05e23' : '#fff',
-            fontSize: '24px',
-            cursor: 'pointer',
-          }}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-            <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/>
-          </svg>
-        </button>
-      </div>
-    );
-  };
+  return (
+    <div style={{
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      backgroundColor: '#1a1a1a',
+      height: '60px',
+      borderTop: '1px solid #333'
+    }}>
+      <button
+        onClick={() => navigate('/')}
+        style={{
+          background: 'none',
+          border: 'none',
+          color: location.pathname === '/' ? '#f05e23' : '#fff',
+          fontSize: '24px',
+          cursor: 'pointer',
+        }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+          <path d="M7 21H3V10l-3 3v-3l9-9 9 9v3l-3-3v11h-4v-7H7v7zm8-11c0-1.1-.9-2-2-2s-2 .9-2 2 .9 2 2 2 2-.9 2-2z"/>
+        </svg>
+      </button>
+      <button
+        onClick={() => navigate('/deals')}
+        style={{
+          background: 'none',
+          border: 'none',
+          color: location.pathname === '/deals' ? '#f05e23' : '#fff',
+          fontSize: '24px',
+          cursor: 'pointer',
+        }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+          <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/>
+        </svg>
+      </button>
+    </div>
 
   return (
     <Router>
