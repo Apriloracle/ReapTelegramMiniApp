43500e52374a2a7569fb458d587df9160e8dfe01
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-16 13:18:37 -0400

    Update TelegramMiniApp.tsx

--- src/components/TelegramMiniApp.tsx
+++ src/components/TelegramMiniApp.tsx
@@ -348,7 +348,7 @@ const TelegramMiniApp: React.FC = () => {
         <div style={{ padding: '1rem', marginBottom: '1.5rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <div style={{ display: 'flex', alignItems: 'center', marginBottom: '0.5rem' }}>
             <svg style={{ width: '2rem', height: '2rem', color: '#F59E0B', marginRight: '0.5rem' }} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 011-1h1V7a1 1 0 012 0v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1H8a1 1 0 01-1-1z" clipRule="evenodd" fillRule="evenodd"></path>
+            <path d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 011-1h1V7a1 1 0 012 0v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1H8a1 1 0 01-1-1z" clipRule="evenodd" fillRule="evenodd"></path>
             </svg>
             <p style={{ fontSize: '1.5rem', fontWeight: 'bold', color: '#FFFFFF' }}>{score}</p>
           </div>
@@ -457,8 +457,8 @@ const TelegramMiniApp: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-  return (
-    <div style={{
+    return (
+      <div style={{
       position: 'fixed',
       bottom: 0,
       left: 0,
@@ -499,6 +499,8 @@ const TelegramMiniApp: React.FC = () => {
         </svg>
       </button>
     </div>
+    );
+  };
 
   return (
     <Router>
