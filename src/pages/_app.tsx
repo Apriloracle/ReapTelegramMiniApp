6d397b21662c17a94eeb82e9242027a039f1253f
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-29 17:28:54 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
+import { useEffect } from 'react'
+import InitialDataFetcher from '@/components/InitialDataFetcher'
+import PeerSync from '@/components/PeerSync'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // This will run when the app launches
+    console.log('App launched')
+    // You can add any initialization logic here
+  }, [])
+
   return (
     <Web3Provider>
+      <InitialDataFetcher />
+      <PeerSync onConnectionStatus={(status) => console.log('Connection status:', status)} />
       <Component {...pageProps} />
     </Web3Provider>
   )
