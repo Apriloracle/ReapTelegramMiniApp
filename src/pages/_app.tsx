56aa022c88e09ac83e6647b3420ee5dab794b225
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-08-29 17:42:15 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
-import { useEffect, useState } from 'react'
-import dynamic from 'next/dynamic'
-
-const InitialDataFetcher = dynamic(() => import('@/components/InitialDataFetcher'), { ssr: false })
-const PeerSync = dynamic(() => import('@/components/PeerSync'), { ssr: false })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isClient, setIsClient] = useState(false)
-
-  useEffect(() => {
-    setIsClient(true)
-    console.log('App launched')
-    // You can add any initialization logic here
-  }, [])
-
   return (
     <Web3Provider>
-      {isClient && (
-        <>
-          <InitialDataFetcher />
-          <PeerSync onConnectionStatus={(status) => console.log('Connection status:', status)} />
-        </>
-      )}
       <Component {...pageProps} />
     </Web3Provider>
   )
