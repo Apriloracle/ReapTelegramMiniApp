62b2804f02cd70489d63bf11e376050b277f9f2d
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-09-04 14:31:22 -0400

    Update InitialDataFetcher.tsx

--- src/components/InitialDataFetcher.tsx
+++ src/components/InitialDataFetcher.tsx
@@ -45,7 +45,7 @@ const InitialDataFetcher: React.FC = () => {
     if (!geolocationData) {
       console.log('Geolocation data not available, using default country code');
       // Use a default country code if geolocation is not available
-      const defaultCountryCode = 'US';
+      const defaultCountryCode = '';
       try {
         const response = await fetch(`https://asia-southeast1-fourth-buffer-421320.cloudfunctions.net/kindredMerchant?countryCode=${defaultCountryCode}`);
         
