47d908bdcaa2d2865564be05c0b7389d2d4f2241
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-09-16 18:14:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const { fileURLToPath } = require('url');
+
+let nextConfig = {
   reactStrictMode: true,
   experimental: {
     esmExternals: true
   },
-}
+  images: {},
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.+(js|jsx|mjs|ts|tsx)$/,
+      use: options.defaultLoaders.babel,
+      include: fileURLToPath(new URL('@electric-sql/pglite', import.meta.url)),
+      type: "javascript/auto",
+    });
 
-module.exports = nextConfig
+    if (!options.isServer) {
+      config.resolve.fallback = { fs: false, module: false, path: false };
+    }
+
+    return config;
+  },
+  transpilePackages: [],
+};
+
+module.exports = nextConfig;
