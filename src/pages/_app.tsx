bf17d6f7b49cb471d7827a6abbfb8ab9617ce938
newbb2 <105810137+newbb2@users.noreply.github.com>
2024-10-11 19:17:04 -0400

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </Head>
-      <Web3Provider>
-        <Component {...pageProps} />
-      </Web3Provider>
-    </>
+    <Web3Provider>
+      <Component {...pageProps} />
+    </Web3Provider>
   )
 }
 
