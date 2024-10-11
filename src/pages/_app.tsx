a7de8fb456b8791c39305b24086d8243402369ec
newbb2 <105810137+newbb2@users.noreply.github.com>
2024-10-11 18:45:00 -0400

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Web3Provider>
-      <Component {...pageProps} />
-    </Web3Provider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
+    </>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <script src="https://unpkg.com/ml5@0.12.2/dist/ml5.min.js"></script>
           <link rel="manifest" href="/manifest.json" />
         </Head>
